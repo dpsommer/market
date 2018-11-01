f9c66b76f64e489ee9e8bd679ccac1fe2ac488ce
Duncan Sommerville <duncan.sommerville@gmail.com>
2018-11-01 19:24:21 -0400

    Remove unnecessary walk of data module

--- market/data/core.py
+++ market/data/core.py
@@ -14,20 +14,18 @@ def loadable(cls):
     As a side-effect, adds the calling class the global GAME_STATE dict for simplicity.
 
     :param cls: the class being decorated
-    :type cls: GameObject
+    :type cls: class[GameObject]
     :return: the decorated class
-    :rtype: GameObject
+    :rtype: class[GameObject]
     """
     cls.is_loadable = True
-    GAME_STATE[cls.__name__] = {}
+    GAME_STATE[cls] = {}
     return cls
 
 
 class GameObject:
     """
     Base game class. Handles core functionality and object marshalling using pickle.
-
-    XXX: all the calls to .__name__ here are ugly
     """
 
     is_loadable = False
@@ -35,24 +33,24 @@ class GameObject:
     def __init__(self, name, uuid=None):
         self.name = name
         self.uuid = uuid or uuid4()
-        GAME_STATE[self.__class__.__name__][name] = self
+        GAME_STATE[self.__class__][name] = self
 
     @classmethod
     def get(cls, name):
-        if name in GAME_STATE[cls.__name__]:
-            return GAME_STATE[cls.__name__][name]
+        if name in GAME_STATE[cls]:
+            return GAME_STATE[cls][name]
         return cls(name)
 
     @classmethod
     def marshal_save(cls):
         with open(os.path.join(os.path.dirname(__file__), '%s.p' % cls.__name__), 'wb') as data_file:
-            pickle.dump(GAME_STATE[cls.__name__], data_file)
+            pickle.dump(GAME_STATE[cls], data_file)
 
     @classmethod
     def marshal_load(cls):
         with open(os.path.join(os.path.dirname(__file__), '%s.p' % cls.__name__), 'rb') as data_file:
             try:
-                GAME_STATE[cls.__name__] = pickle.load(data_file)
+                GAME_STATE[cls] = pickle.load(data_file)
             except EOFError:
                 pass  # if file is empty
 
--- market/util/data.py
+++ market/util/data.py
@@ -1,41 +1,12 @@
 import io
-import importlib
 import pickle
-import pkgutil
-import pyclbr
 
-from market import data as data_module
-from market.data.core import GameObject, GAME_STATE
-
-
-def find_loadable_classes(package):
-    """
-    Recursively walk through the given package and find all loadable GameObject subclasses.
-
-    :param package: package (name or actual module)
-    :type package: str | module
-    :rtype: list[GameObject]
-    """
-    if isinstance(package, str):
-        package = importlib.import_module(package)
-    loadable_classes = []
-    for loader, name, is_pkg in pkgutil.walk_packages(path=package.__path__):
-        full_name = package.__name__ + '.' + name
-        module = importlib.import_module(full_name)
-        # get name here because returned values are pyclbr class objects
-        cls = [_.name for _ in pyclbr.readmodule(full_name).values()]
-        for class_name in cls:
-            c = getattr(module, class_name)
-            if issubclass(c, GameObject) and c.is_loadable:
-                loadable_classes.append(c)
-        if is_pkg:
-            return loadable_classes + find_loadable_classes(full_name)
-    return loadable_classes
+from market.data.core import GAME_STATE
 
 
 class Data:
 
-    LOADABLE_CLASSES = find_loadable_classes(data_module)
+    LOADABLE_CLASSES = GAME_STATE.keys()
 
     @staticmethod
     def save():
@@ -62,12 +33,12 @@ class MockData:
     @staticmethod
     def save():
         for cls, stream in MockData.DATA_STREAMS.items():
-            pickle.dump(GAME_STATE[cls.__name__], stream)
+            pickle.dump(GAME_STATE[cls], stream)
 
     @staticmethod
     def load():
         for cls, stream in MockData.DATA_STREAMS.items():
             try:
-                GAME_STATE[cls.__name__] = pickle.load(stream)
+                GAME_STATE[cls] = pickle.load(stream)
             except EOFError:
                 pass
