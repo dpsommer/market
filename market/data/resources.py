0cdf93feb7af1bebe4ffa53a93f715aad871310c
Duncan Sommerville <duncan.sommerville@gmail.com>
2018-11-03 10:48:07 -0400

    Update to drop object hashing and comparisons

--- market/data/resources.py
+++ market/data/resources.py
@@ -21,21 +21,50 @@ class Resource(GameObject):
         self._drop_table.update(drops)
 
     def add_drop(self, drop, rate=DEFAULT_DROP_RATE):
+        """
+        Add a drop object to the resources drop table.
+
+        :param drop: the object representing the number and type of item to drop
+        :type drop: Resource.Drop
+        :param rate: the rate at which the item will drop
+        :type rate: int | float
+        """
         self._drop_table[drop] = rate
 
     class Drop:
         DECIMAL_PLACES = 3
 
         def __init__(self, item, lower_bound=1, upper_bound=1):
+            if upper_bound < lower_bound or upper_bound < 0 or lower_bound < 0:
+                raise self.BoundingError("Invalid upper or lower bound defined for Drop object, "
+                                         "given lower: %d, upper: %d" % (lower_bound, upper_bound))
             self.item = item
             self.lower_bound = lower_bound
             self.upper_bound = upper_bound
 
         def __hash__(self):
-            return self.item.__hash__()  # XXX: should drops with different bounds be equal?
+            """
+            Derived from https://stackoverflow.com/a/11742634
+            """
+            h = hash(self.item) * 31 + self.lower_bound
+            return h * 31 + self.upper_bound
 
         def __eq__(self, o):
-            return self.item == o.item
+            """
+            Equality comparisons for drops should check the represented item
+            and both upper and lower bounds for drop amounts.
+
+            Item objects, like all GameObjects, are compared by UUID.
+
+            :param o: the drop to compare against
+            :type o: Resource.Drop
+            """
+            return (self.item == o.item
+                    and self.lower_bound == o.lower_bound
+                    and self.upper_bound == o.upper_bound)
+
+        class BoundingError(Exception):
+            pass
 
     class InvalidResourceType(Exception):
         pass
